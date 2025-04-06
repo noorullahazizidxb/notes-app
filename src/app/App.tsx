202a90d1d5f13a4ed510a8df0f955e271848da49
noorullahazizidxb <146918490+noorullahazizidxb@users.noreply.github.com>
2025-04-06 14:30:59 +0430

    Update App.tsx
    Removed unused imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+
 import { useNotesStore } from '../store/notesStore';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './root';
