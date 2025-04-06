b7610e3ff2ca9bb77cbee3e2e99b9c399f42d46a
Noorullah Azizi <Noorullah.Azizi2040@gmail.com>
2025-04-06 15:11:13 +0430

    Deployment Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+    base: mode === 'production' ? '/' : '/', // Adjust base if needed for production
+    build: {
+      outDir: 'dist',
+    },
+  };
 });
