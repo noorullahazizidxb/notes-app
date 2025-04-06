9f3449a5fd95c75113c40fdd008e326405f80c2c
noorullahazizidxb <146918490+noorullahazizidxb@users.noreply.github.com>
2025-04-06 14:31:57 +0430

    Update Editor.tsx
    Removed unused imports

--- src/app/routes/Editor.tsx
+++ src/app/routes/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useNotesStore } from '../../store/notesStore';
 import ReactQuill from 'react-quill';
